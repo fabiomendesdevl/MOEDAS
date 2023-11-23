e5da6c9268b37e37776dcd09ad77eef8b1b272d4
Fabio Mendes <fabiomendesdev@outlook.com>
2023-11-23 16:27:06 -0300

    MOEDAS $ ETC

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        decimal valor = 10589.25m;
+        
+       // Console.WriteLine(valor.ToString());
+        Console.WriteLine(
+            valor.ToString(
+                "C",
+        CultureInfo.CreateSpecificCulture("pt-BR")));
+    }   
+}s
